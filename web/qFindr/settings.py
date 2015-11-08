b33a207a2d8c715436f735c6f954237c9d1a5b7e
Jason Yao <jasony.edu@gmail.com>
2015-11-08 10:46:49 -0500

    Added postgreSQL database access for the application

--- web/qFindr/settings.py
+++ web/qFindr/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'qFindr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'qFindr',
+        'USER': 'qFindr',
+        'PASSWORD': 'INSERT_QFINDR_USER_PASSWORD_HERE',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
