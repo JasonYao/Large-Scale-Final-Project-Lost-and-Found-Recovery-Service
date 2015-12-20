59ce18f8d51de28e297ab05fcf837e4620f28d4f
Chisom Mba <cmba93@yahoo.com>
2015-12-19 21:54:29 -0500

    Added delete item route and fixed some bugs

--- web/finder/my_qrcode/urls.py
+++ web/finder/my_qrcode/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'^items/(?P<user_id>[0-9]+)/(?P<item_id>[0-9]+)/$', views.item, name='item'),
     url(r'^add/$', views.add_item, name='item_add'),
     url(r'^items/(?P<item_id>[0-9]+)/edit/$', views.edit_item, name='item_edit'),
+    url(r'^items/(?P<item_id>[0-9]+)/delete/$', views.delete_item, name='item_delete'),
     url(r'^generate/(?P<item_id>[0-9]+)/$', views.generate, name='generate'),
     url(r'^found/(?P<user_id>[0-9]+)/(?P<item_id>[0-9]+)/$', views.found, name='found'),
     url('^', include('django.contrib.auth.urls')),
