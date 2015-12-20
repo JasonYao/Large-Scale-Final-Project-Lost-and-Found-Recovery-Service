bce948e730462a6f9850bd79ed5bd1f4bda7865d
Chisom Mba <cmba93@yahoo.com>
2015-12-20 17:22:18 -0500

    Fixed static files directing

--- web/finder/finder/settings.py
+++ web/finder/finder/settings.py
@@ -116,9 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-#STATIC_URL = 'static/'
-STATIC_URL = os.path.join(BASE_DIR, "static/")
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_URL = '/static/'
+#STATIC_URL = os.path.join(BASE_DIR, "static/")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+#print STATIC_ROOT
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/profile/'
