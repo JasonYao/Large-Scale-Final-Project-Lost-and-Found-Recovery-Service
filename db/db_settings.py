ec1209f76a5d1bf84ab741022a0af6686da2d6ee
Chisom Mba <cmba93@yahoo.com>
2015-12-21 19:28:56 -0500

    Temp live server db fix

--- db/db_settings.py
+++ db/db_settings.py
@@ -6,7 +6,7 @@ DATABASES = {
         'NAME': 'finder',
         'USER': 'finder',
         'PASSWORD': 'Paz*1C40!3h@SB4dxg2tR%R2n5EnriLgBZgMpAjdpOdoKum#8M4UgLFjQ%h39oRXUur6nQt6v4aEPyA$E%9aqArsYOfe4IM@H49M',
-        'HOST': '',
+        'HOST': '127.0.0.1',
         'PORT': '',
     },
     'db1': {
@@ -14,7 +14,7 @@ DATABASES = {
         'NAME': 'finder',
         'USER': 'finder',
         'PASSWORD': 'Paz*1C40!3h@SB4dxg2tR%R2n5EnriLgBZgMpAjdpOdoKum#8M4UgLFjQ%h39oRXUur6nQt6v4aEPyA$E%9aqArsYOfe4IM@H49M',
-        'HOST': '',
+        'HOST': '127.0.0.1',
         'PORT': '',
     },
     'db2': {
@@ -22,10 +22,21 @@ DATABASES = {
         'NAME': 'finder',
         'USER': 'finder',
         'PASSWORD': 'Paz*1C40!3h@SB4dxg2tR%R2n5EnriLgBZgMpAjdpOdoKum#8M4UgLFjQ%h39oRXUur6nQt6v4aEPyA$E%9aqArsYOfe4IM@H49M',
-        'HOST': '',
+        'HOST': '127.0.0.1',
         'PORT': '',
     },
 }
+
+# temp live server fix
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'finder',
+    'USER': 'finder',
+    'PASSWORD': 'Paz*1C40!3h@SB4dxg2tR%R2n5EnriLgBZgMpAjdpOdoKum#8M4UgLFjQ%h39oRXUur6nQt6v4aEPyA$E%9aqArsYOfe4IM@H49M',
+    'HOST': '',
+    'PORT': '',
+}
+DATABASES['auth_db'] = DATABASES['db1'] = DATABASES['db'] = DATABASES['default']
 
 # Database routers go here:
 DATABASE_ROUTERS = ['my_qrcode.routers.UserRouter']
