446dff2ba640629e72373a26c49f0bb5636d7cd5
Chisom Mba <cmba93@yahoo.com>
2015-12-21 21:59:19 -0500

    Front end static files fix

--- web/finder/finder/settings.py
+++ web/finder/finder/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATIC_URL = os.path.join(BASE_DIR, "static/")
 #STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = "/static/" # set the static root to the root static folder
 #print STATIC_ROOT
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
